891b05bc23b3aa7e01975ec73c05319d224257e2
de6igz <Solopov2003@gmail.com>
2023-12-14 19:12:04 +0300

    работает!!!

--- src/services/search.js
+++ src/services/search.js
@@ -13,41 +13,78 @@ function searchCharacter() {
     }
 
     // Формируем URL для запроса
-    const apiUrl = `https://narutodb.xyz/api/character/search?name=${encodeURIComponent(characterName)}`;
+    // const apiUrl = `https://narutodb.xyz/api/character/search?name=${encodeURIComponent(characterName)}`;
+    const apiUrl = 'https://narutodb.xyz/api/character?page=1&limit=1431'; // Замените на реальный URL вашего API
 
     // Отправляем запрос на сервер
     fetch(apiUrl)
         .then(response => response.json())
         .then(data => {
-            // Скрываем все блоки
             characterContainer.style.display = 'none';
             resultContainer.style.display = 'none';
-
-            // Проверяем, что сервер вернул статус 200
-            if (data.name === characterName) {
-                // Выводим информацию о персонаже
-                const character = data; // Предполагаем, что возвращается только один результат
-                console.log(character)
-                if (character) {
-                    const characterBlock = createCharacterBlock(character);
-                    characterContainer.innerHTML = '';
-                    characterContainer.appendChild(characterBlock);
-                    characterContainer.style.display = 'block';
-                } else {
-                    resultContainer.textContent = 'Персонаж не найден';
-                    resultContainer.style.display = 'block';
-                }
-            } else {
-                console.log(data)
-                resultContainer.textContent = 'Ошибка при выполнении запроса';
-                resultContainer.style.display = 'block';
+            if (!data || !data.characters || !Array.isArray(data.characters)) {
+                console.error('Invalid data format:', data);
+                return;
             }
+
+            const characters = data.characters;
+            const characterBlocks = [];
+
+            characters.forEach(character => {
+                try {
+                    if (character.name.includes(characterName)) {
+                        console.log(character);
+                        const characterBlock = createCharacterBlock(character);
+                        characterBlocks.push(characterBlock);
+                    }
+                } catch (error) {
+                    console.error('Error creating character block:', error);
+                }
+            });
+
+            // Очищаем контейнер и добавляем все блоки одновременно
+            characterContainer.innerHTML = '';
+            characterBlocks.forEach(characterBlock => {
+                characterContainer.appendChild(characterBlock);
+            });
+
+            // Показываем контейнер
+            characterContainer.style.display = 'block';
         })
-        .catch(error => {
-            console.error('Ошибка:', error);
-            resultContainer.textContent = 'Произошла ошибка';
-            resultContainer.style.display = 'block';
-        });
+        .catch(error => console.error('Error fetching characters:', error));
+
+    // .then(response => response.json())
+    // .then(data => {
+    //     console.log(data);
+    //     // Скрываем все блоки
+    //     characterContainer.style.display = 'none';
+    //     resultContainer.style.display = 'none';
+    //     console.log(data)
+    //     // Проверяем, что сервер вернул статус 200
+    //     if (data.name.contains(characterName)) {
+    //         // Выводим информацию о персонаже
+    //         const character = data; // Предполагаем, что возвращается только один результат
+    //         console.log(character)
+    //         if (character) {
+    //             const characterBlock = createCharacterBlock(character);
+    //             characterContainer.innerHTML = '';
+    //             characterContainer.appendChild(characterBlock);
+    //             characterContainer.style.display = 'block';
+    //         } else {
+    //             resultContainer.textContent = 'Персонаж не найден';
+    //             resultContainer.style.display = 'block';
+    //         }
+    //     } else {
+    //         console.log(data)
+    //         resultContainer.textContent = 'Ошибка при выполнении запроса';
+    //         resultContainer.style.display = 'block';
+    //     }
+    // })
+    // .catch(error => {
+    //     console.error('Ошибка:', error);
+    //     resultContainer.textContent = 'Произошла ошибка';
+    //     resultContainer.style.display = 'block';
+    // });
 }
 
 function createCharacterBlock(character) {
