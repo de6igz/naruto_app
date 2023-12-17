e9071547a16c1b610ac665191756045b5a9e74d7
de6igz <Solopov2003@gmail.com>
2023-12-17 19:29:53 +0300

    не регается сервис воркер

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 3000;
+const cors = require('cors')
+const bodyParser = require("body-parser");
+app.use(cors());
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+const port = 3001;
+
+const fs = require('fs');
+
 
 // Указываем Express обслуживать статические файлы из папки public
 app.use(express.static(__dirname +'/src'));
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/src/ui/StartScreen', 'StartScreen.html'));
+});
+app.post("/subscription", (req, res) => {
+    const pushSubscription = req.body;
+    fs.writeFileSync("subscription.json", JSON.stringify(pushSubscription));
+    res.status(200).send("OK");
 });
 
 app.get('/characters', (req, res) => {
